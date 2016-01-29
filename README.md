5023adaa5c2bf316f55b85d24a3f9679d0e72730
Matthew Kaufer <matthew@kaufer.org>
2016-01-29 19:17:59 +0000

    Frameworking, tracking chats & messages, renaming stuff, etc.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# Messer
+# Messer-GUI
 
-Command-line messaging for Facebook Messenger
+Command-line messaging for Facebook Messenger, now with a pretty GUI
+
+**Note: This project is not yet finished, and should not be used. In the meantime, you can use [Messer](http://github.com/mjkaufer/Messer).**
 
 ## Installation
 
-Install `messer` globally with `npm install messer -g`, so you can use the command in any context.
+Install `messer-gui` globally with `npm install messer-gui -g`, so you can use the command in any context.
 
 ## Setup
 
 Make sure you are running Node 4.x
 
-If you want to log in with your credentials stored in a file, do the following - otherwise, you'll log in by typing in your credentials each time you run Messer
+If you want to log in with your credentials stored in a file, do the following - otherwise, you'll log in by typing in your credentials each time you run Messer-GUI
 
 Create a `config.json` somewhere. Inside of the `config.json`, add
 
@@ -24,26 +26,13 @@ Fill in the email you use for Facebook, along with your Facebook password
 
 ## Usage
 
-If you stored your credentials in a json, simply type `messer path/to/config.json`, replacing `path/to/config.json` with the path to your `config.json` Otherwise, type `messer` and input your email and password as you are prompted for them. The password will not be visible as you type it in.
+If you stored your credentials in a json, simply type `messer-gui path/to/config.json`, replacing `path/to/config.json` with the path to your `config.json` Otherwise, type `messer-gui` and input your email and password as you are prompted for them. The password will not be visible as you type it in.
 
-Once you're logged in, you'll see a REPL.
-
-Currently, the only supported commands are `message` and `reply`
-
-### Syntax:
-
-`message "[user]" [message]`
-
-The quotes around the username are mandatory. An example might be `message "Matthew" hello world!`
-
-When sending a message, Messer picks the closest match to what you type in as `user`. For this reason, you should probably use last names if you want to avoid accidentally texting someone.
-
-`reply [message]`
-
-This will reply to the last message you received. Note: this will only work if you have received at least one message through Messer.
+Once you're logged in, you'll see a GUI. This has yet to be designed, so more instructions will come.
 
 ## Todo
 
+* **Actually implement the GUI part**
 * Track received messages from users, and give the user the ability to look at them with a command
 * Make a Messenger-esque UI in the terminal
 	* Using `blessed`
@@ -56,6 +45,8 @@ This will reply to the last message you received. Note: this will only work if y
 ## Warnings
 
 facebook-chat-api@1.0.6 relies on a version of node which requires basic ES6 support - use facebook-chat-api@1.0.5 if you have an older version of node
+
+Windows is a bit glitchy with `blessed`, so try using Linux or OSX if you can.
 
 ## Contributing
 
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 #! /usr/bin/env node
-
 var login = require("facebook-chat-api");
-var repl = require('repl')
 var lastThread = null;
-var unrenderableMessage = ", unrenderable in Messer :("
 
 if(process.argv.length < 3){//user didn't store credentials in JSON, make them manually enter credentials
 
@@ -39,131 +36,149 @@ function authenticate(credentials){//where credentials is the user's credentials
 	login(credentials, function(err, api) {
 
 		if(err) return console.error(err);
+		
+		var me = api.getCurrentUserID()
+
+		var blessed = require('blessed')
 
 		console.log("Logged in as " + credentials.email)
 
 		api.setOptions({
 			logLevel: "silent"
 		})
+		
+		var friendIDMap = {
+			//friendId: friendName
+		}
+		
+		function findNames(friendIDArray, callback){
+			api.getUserInfo(friendIDArray, function(err, friends) {
+				if(err) return console.error(err);
+		
+				var friendArray = []
+		
+				for(var friendID in friends) {
+					friendArray.push(friends[friendID])
+					friendIDMap[friendID] = friends[friendID]
+				}
+				// console.log(friends)
+				if(callback)
+					callback(err, friendArray)
+			});
+		}
+		
+		var chats = {
+			/*
+			threadID: {
+				participants: [participantIDs,...],
+				name: "chatname",
+				messageHistory: [
+					//messages, where index 0 is first and last index is most recent
+				]
+			}
+			
+			*/
+		}
+		
+		var mostRecent = [
+			//threadIDs sorted from most recent at 0 to least recent at index.length-1	
+		]
+		
+		var startingChatList = 5
+		
+		var chatLoadComplete = new Array(startingChatList)
+		for(var i = 0; i < chatLoadComplete.length; i++)
+			chatLoadComplete[i] = false
+
+		api.getThreadList(0, startingChatList, function(err, threads){
+			if(err) return console.log(err)
+			for(var i = 0; i < threads.length; i++){
+				var thread = threads[i]
+				// console.log("------------")
+				// console.log(thread.name)
+				// console.log('"' + thread.snippet + '"')
+				
+				// console.log("------------")
+				// console.log(thread)
+				try{
+				mostRecent.push(thread.threadID)
+				} catch(e){console.log(e)}
+				
+				(function(threadCopy, index){
+					findNames(threadCopy.participants.filter(function(e){return e != me}), function(err, arr){
+						if(err){ return console.log(err)}
+
+						chats[threadCopy.threadID] = {
+							participants: threadCopy.participants,
+							name: threadCopy.name || arr[0].name,
+							messageHistory: []
+						}
+						chatLoadComplete[index] = true
+						if(chatLoadComplete.every(function(e){return e == true}))
+							createUI()
+						else{
+							var percentage = chatLoadComplete.filter(function(e){return e}).length / chatLoadComplete.length * 100
+							console.log("Loading: " + percentage + "%")
+						}
+					})
+				})(thread, i)
+				
+				
+				
+				
+			}
+		})
+		
+
 
 		api.listen(function cb(err, message) {
 			if(err)
 				return console.log(err)
 			
 			var from = message.threadName
+			var threadID = message.threadID
 
-			if(message.participantNames && message.participantNames.length > 1)
-				from = "'" + from + "'" + " (" + message.senderName + ")"
-
-			process.stderr.write("\007");//makes a beep
-
-			var messageBody = null
-
-
+			var formattedMessage = message.senderName
+			
 			if(message.type != "message"){
 				return
-			}
-			else if(message.body !== undefined && message.body != ""){
-				// console.log("New message from " + from + " - " + message.body)
-				messageBody = " - " + message.body
-			} 
-
-			if(message.attachments.length == 0)
-				console.log("New message from " + from + (messageBody || unrenderableMessage))
-			else{
+			} else if(message.body !== undefined && message.body != ""){
+				formattedMessage += ": " + message.body
+			} else if(message.attachments.length > 0){
 				var attachment = message.attachments[0]//only first attachment
 				var attachmentType = attachment.type.replace(/\_/g," ")
-				console.log("New " + attachmentType + " from " + from + (messageBody || unrenderableMessage))
+				formattedMessage += " sent a " + attachmentType
 			}
 			
-			lastThread = message.threadID;
-
-		});
-
-		var quoteReg = /(".*?")(.*)/g
-		repl.start({
-			ignoreUndefined: true,
-			eval: function(cmd, context, filename, callback){
-				var ndex = cmd.lastIndexOf("\n")
-				ndex = ndex == -1 ? cmd.length : ndex
-
-				cmd = cmd.substring(0, ndex)
-
-
-				if(cmd.toLowerCase() == "help"){
-					showHelp()
-					return callback(null)
-				} else if(cmd.toLowerCase().indexOf("message") == 0){
-					cmd = cmd.substring("message".length).trim()
-
-					if(cmd.match(quoteReg) == null){
-						console.log("Invalid message - check your syntax")
-						showHelp()
-						return callback(null)
-					}
-
-					var decomposed = quoteReg.exec(cmd)
-					var to = decomposed[1].replace(/"/g,"")
-					var message = decomposed[2].trim()
-					if(message.length == 0){
-						console.log("No message to send - check your syntax")
-						showHelp()
-						return callback(null)
-					}
-					api.getUserID(to, function(err, data){
-						if(err){
-							console.log("ERROR!", err)
-							return callback(null)
-						}
-						var threadID = data[0].userID;
-						api.sendMessage(message, threadID, function(err){
-							if(err){
-								console.log("ERROR!", err)
-								return callback(null)
-							}
-							console.log("Sent message to " + to);
-										return callback(null)
-						});
-					
-					});
-
-				} else if(cmd.toLowerCase().indexOf("reply") == 0){
-					if(lastThread === null){
-						console.log("Error - can't reply to messages you haven't yet received! You need to receive a message before using `reply`!");
-						return callback(null);
-					}
-
-					var body = cmd.substring("reply".length).trim();
-
-					api.sendMessage(body, lastThread, function(err, data){
-
-						if(err){
-							console.log("ERROR!")
-							console.log(err)
-							return callback(null)
-						}
-
-						console.log("Successfully replied!")
-						return callback(null)
-					})
-
-
-				} else {
-					console.log("Invalid command - check your syntax")
-					showHelp()
-					return callback(null)	
+			if(mostRecent.indexOf(threadID) != -1)
+				mostRecent.splice(0, 0, mostRecent.splice(mostRecent.indexOf(threadID), 1)[0] );
+			else{
+				mostRecent.unshift(threadID)
+				chats[threadID] = {
+					participants: message.participantIDs,
+					name: message.threadName,
+					messageHistory: []
 				}
-
+				findNames(message.participantIDs)
 			}
-		})
+			
+			chats[threadID].messageHistory.push(formattedMessage)//add message to chat history
 
-		function showHelp(){
-			console.log("Commands:\n" + 
-				" message \"[user]\" [message]\n" +
-				" reply [message]"
-			)
+			
+			process.stderr.write("\007");//makes a beep
+			
+			
+		});
+		
+		function createUI(){
+			console.log("Ready!")
+			console.log(chats)
+			console.log("\n----\n")
+			console.log(friendIDMap)
 		}
 
 	});
 }
+
+
 		
